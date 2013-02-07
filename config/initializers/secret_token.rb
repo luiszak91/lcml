fcc1f76701f28174df5a5bdb0001922c6d6d51b0
luiszak91 <lmontoya@dacompsc.com>
2013-02-07 10:22:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lcml::Application.config.secret_token = '1e738d67b6c2da773488250a9059ff9df347baee41274ca82c64804b1f94f55733d2e3a54039d5fe6859d7083c90738ebf0966dc7988943c07d9aa9261123543'
