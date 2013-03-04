6f1e5b4e8bee2762d5fd8f9fc646d1c9ec6e0429
luiszak91 <lmontoya@dacompsc.com>
2013-03-04 11:30:35 -0600

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "user pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: "Sign") }
+    #tiene k ver algo con el <title>Ruby on Rails Tutorial Sample App | <%= yield(:title) %></title>
+    it { should have_selector('title', text: 'Singup') }
+  end
+end
